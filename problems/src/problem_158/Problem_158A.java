61fbc0f9e3e79b7c4cd647154f96b47d3c9af439
AlbertHambardzumyan <hambardzumyan.albert@gmail.com>
2017-03-26 17:18:33 +0200

    Problem 158A - reformat code.

--- problems/src/problem_158/Problem_158A.java
+++ problems/src/problem_158/Problem_158A.java
@@ -7,18 +7,18 @@ import java.util.Scanner;
  * @author Albert Hambardzumyan
  */
 public class Problem_158A {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int participants = in.nextInt();
         int place = in.nextInt();
 
         int[] points = new int[participants];
-        for (int i = 0; i < points.length; i++){
+        for (int i = 0; i < points.length; i++) {
             points[i] = in.nextInt();
         }
 
         int count = 0;
-        int temp = points[place-1];
+        int temp = points[place - 1];
         for (int point : points) {
             if (point != 0 && point >= temp)
                 count++;
