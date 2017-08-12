1fd7ef6635ebb2a6ef732d65c10b467ab251c065
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-08-12 19:38:53 +0200

    Problem 766A  - A. Mahmoud and Longest Uncommon Subsequence.

---
+++ problems/src/problem_766/Problem_766A.java
@@ -0,0 +1,19 @@
+package problem_766;
+
+import java.util.Scanner;
+
+/**
+ * @author Albert Hambardzumyan
+ */
+public class Problem_766A {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.nextLine();
+        String b = in.nextLine();
+
+        int result;
+        result = a.equals(b) ? -1 : Integer.max(a.length(), b.length());
+
+        System.out.print(result);
+    }
+}
